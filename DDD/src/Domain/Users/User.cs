859673ad2340d588f673de1a1311ccfb7f9599ac
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-23 18:53:53 +0100

    Update User.cs

--- DDD/src/Domain/Users/User.cs
+++ DDD/src/Domain/Users/User.cs
@@ -1,7 +1,13 @@
-﻿namespace Domain.Users
-{
-    public class User
-    {
+﻿using Domain.Abstractions;
 
+namespace Domain.Users
+{
+    public class User : Entity
+    {
+        private User(Guid id, Name name) : base(id)
+        {
+            name = name;
+        }
+        public Name Name { get; private set; }
     }
 }
