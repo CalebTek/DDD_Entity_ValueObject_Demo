d2d66671e900f4e9037eac23820086f1e4dfb00d
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-24 01:18:16 +0100

    added name tests

--- DDD/tests/Domain.UnitTests/Users/NameTests.cs
+++ DDD/tests/Domain.UnitTests/Users/NameTests.cs
@@ -1,4 +1,5 @@
 using Domain.Users;
+using FluentAssertions;
 using Xunit;
 
 namespace Domain.UnitTests.Users
@@ -11,6 +12,39 @@ namespace Domain.UnitTests.Users
         public void Constructor_Should_ThrowArgumentException_WhenValueIsInvalid(string? value)
         {
             Name Action() => new Name(value);
+
+            // Assert
+            FluentActions.Invoking(Action).Should().Throw<ArgumentException>()
+                .Which.ParamName.Should().Be("value");
         }
+
+        [Fact]
+        public void Equals_Should_ReturnTrue_WhenNamesAreEqual()
+        {
+            // Arrange
+            var name1 = new Name("John");
+            var name2 = new Name("John");
+
+            // Act
+            bool result = name1.Equals(name2);
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Equals_Should_ReturnFalse_WhenNamesAreDifferent()
+        {
+            // Arrange
+            var name1 = new Name("John");
+            var name2 = new Name("Jane");
+
+            // Act
+            bool result = name1.Equals(name2);
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
     }
 }
