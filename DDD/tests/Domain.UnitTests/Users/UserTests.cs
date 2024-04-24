9f8399ada8aaf1fa67953ba67c3ac2d4c9b0f127
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-24 10:46:54 +0100

    Create UserTests.cs

---
+++ DDD/tests/Domain.UnitTests/Users/UserTests.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Users;
+using FluentAssertions;
+
+namespace Domain.UnitTests.Users
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Create_Should_ReturnUser_WhenNameIsValid()
+        {
+            // Arrange
+            var name = new Name("John Doe");
+
+            // Act
+            var user = User.Create(name);
+
+            // Assert
+            user.Should().NotBeNull();
+        }
+    }
+}
