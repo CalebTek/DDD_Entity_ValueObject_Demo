b07aa26c6d8b6fd31d2392f625aa71494928320b
CalebTek <olawale.odeyemi@decagon.dev>
2024-04-24 01:24:55 +0100

    Update Name.cs

--- DDD/src/Domain/Users/Name.cs
+++ DDD/src/Domain/Users/Name.cs
@@ -1,13 +1,29 @@
-﻿namespace Domain.Users
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Domain.Users
 {
     public sealed record Name
     {
         public Name(string? value)
         {
-            ArgumentException.ThrowIfNullOrEmpty(value, nameof(value));
+            //ArgumentException.ThrowIfNullOrEmpty(value, nameof(value));
+            Ensure.NotNullOrEmpty(value);
             Value = value;
         }
 
         public string Value { get;}
+    }
+
+    public static class Ensure
+    {
+        public static void NotNullOrEmpty([NotNull] string? value,
+            [CallerArgumentExpression("value")] string? paramName = null)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+        }
     }
 }
